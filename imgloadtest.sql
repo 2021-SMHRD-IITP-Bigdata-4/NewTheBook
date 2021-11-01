02273ef1b55bae712c0ac3e9bc9569dbb9e77e98
노이준 <86701488+Noijun@users.noreply.github.com>
2021-11-01 19:33:55 +0900

    Add files via upload

---
+++ imgloadtest.sql
@@ -0,0 +1,7 @@
+select user_pic from users where user_id = 'test1';
+
+select * from users;
+
+insert into users values('test2',null,'test','1995-01-01',26,'pooh','m','t','gganboo.jpg',sysdate,'1');
+
+commit;
